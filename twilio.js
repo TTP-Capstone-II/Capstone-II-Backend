34ea8ecd232354b2624b3863af01fc6b98a798bb
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-08-14 11:02:41 -0400

    Added comments and renaming

--- twilio.js
+++ twilio.js
@@ -6,8 +6,10 @@ const client = twilio(accountSid, authToken);
 
 const generateTurnToken = async (req, res) => {
   try {
+    // Create Identity for twilio token
     const identity = req.query.identity || "guest-" + Date.now();
 
+    // Create Twilio Token
     const token = new twilio.jwt.AccessToken(
       accountSid,
       process.env.TWILIO_API_KEY,
@@ -16,14 +18,16 @@ const generateTurnToken = async (req, res) => {
     );
 
     // Add TURN grant
+    // Probably unnecessary
     const grant = new twilio.jwt.AccessToken.VideoGrant();
     token.addGrant(grant);
 
-    const iceServers = await client.tokens.create(); // Twilio returns ICE servers here
+    // Create a token instance
+    const tokenInstance = await client.tokens.create(); 
 
     res.json({ 
       token: token.toJwt(),
-      iceServers: iceServers.ice_servers // This is what you pass to RTCPeerConnection
+      iceServers: tokenInstance.ice_servers 
     });
   } catch (error) {
     console.error('Error generating TURN token:', error);
